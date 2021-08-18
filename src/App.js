2f5843a6e2bdfd2493aefee9fa5fc2cb8ff3c045
Edward-Shawn <sbh3834@naver.com>
2021-08-18 23:14:38 +0900

    리액트 라우터 작업

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import Rooms from "./Rooms";
 import "./App.css";
 import { faCoffee, faSearch } from "@fortawesome/free-solid-svg-icons";
 import FontIcon from "./component/icons/FontIcon";
@@ -5,10 +8,33 @@ import FontIcon from "./component/icons/FontIcon";
 function App() {
   console.log(FontIcon);
   return (
-    <div className="App">
-      <FontIcon text="아이콘" icons={faSearch} />
-    </div>
+    <BrowserRouter>
+      {" // (1) "}
+
+      <div style={{ padding: 20, border: "5px solid gray" }}>
+        <Link to="/">홈</Link>
+        {/* // (2) */}
+        <br />
+        <Link to="/photo">사진</Link>
+        <br />
+        <Link to="rooms">방 소개</Link>
+        <br />
+        <Switch>
+          {"// (3) "}
+          <Route exact path="/" component={Home} /> (4)
+          <Route path="/photo" component={Photo} />
+          <Route path="/rooms" component={Rooms} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
+}
+
+function Home({ match }) {
+  return <h2>여기는 홈페이지입니다.</h2>;
+}
+function Photo({ match }) {
+  return <h2>여기서 사진을 감상하세요.</h2>;
 }
 
 export default App;
