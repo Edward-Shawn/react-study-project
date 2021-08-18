837f5c4dc6ba5a071e56cee11b6eac2b9f2a1db6
Edward-Shawn <sbh3834@naver.com>
2021-08-18 23:33:20 +0900

    [DEV-001]브랜치 커밋 확인용

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
     <BrowserRouter>
       <div style={{ padding: 20, border: "5px solid gray" }}>
         <Link to="/">홈</Link>
-
         <br />
         <Link to="/photo">사진</Link>
         <br />
