f49d7d9c4a326f3d00b4483199d5451eb8764217
Edward-Shawn <sbh3834@naver.com>
2023-12-21 23:50:28 +0900

    쇼핑몰 레이아웃 및 mock데이터 생성 및 리스트에 적용

--- online_store2023-12-20/src/App.js
+++ online_store2023-12-20/src/App.js
@@ -1,7 +1,14 @@
-import { Button, Navbar, Nav, Container } from "react-bootstrap";
+import { Button, Navbar, Nav, Container, Row, Col } from "react-bootstrap";
 import "./App.css";
+import { a, b, data } from "./data";
+import imgbg from "./img/bg.jpg";
+import { useState } from "react";
 
 function App() {
+  let [shoes] = useState(data);
+
+  console.log(shoes);
+
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
@@ -14,6 +21,36 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+      <div
+        className="main_bg"
+        style={{ backgroundImage: "url(" + imgbg + ")" }}
+      ></div>
+
+      <Container>
+        <Row>
+          <Col>
+            <img src={process.env.PUBLIC_URL + "/bg.jpg"} width={"80%"} />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].content}</p>
+          </Col>
+          <Col>
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width={"80%"}
+            />
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[0].content}</p>
+          </Col>
+          <Col>
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width={"80%"}
+            />
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[0].content}</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ online_store2023-12-20/src/data.js
@@ -0,0 +1,27 @@
+const a = ["kim", 20]; //배열형
+const b = { name: "kim", age: 20 }; // object 자료형 - 자료형은 name, age처럼 자료형의 이름을 지정해야함
+
+let data = [
+  {
+    id: 0,
+    title: "White and Black",
+    content: "Born in France",
+    price: 120000,
+  },
+
+  {
+    id: 1,
+    title: "Red Knit",
+    content: "Born in Seoul",
+    price: 110000,
+  },
+
+  {
+    id: 2,
+    title: "Grey Yordan",
+    content: "Born in the States",
+    price: 130000,
+  },
+];
+
+export { a, b, data };
