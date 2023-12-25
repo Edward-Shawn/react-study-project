3ce780092797f14a34f8eb0027ad2f9d6c966e23
Edward-Shawn <sbh3834@naver.com>
2023-12-25 23:48:28 +0900

    쇼핑몰 상세페이지 작업

--- online_store2023-12-20/src/pages/DetailPage/index.js
+++ online_store2023-12-20/src/pages/DetailPage/index.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import { Button, Navbar, Nav, Container, Row, Col } from "react-bootstrap";
-const DetailPage = () => {
+const DetailPage = (props) => {
   return (
     <>
       <Container>
-        <Row>상세 페이지</Row>
+        <Row>
+          <div className="col-md-6">
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width={"100%"}
+            />
+          </div>
+          <div className="col-md-6 mt-4">
+            <h4 className="pt-5">{props.shoes[0].title}</h4>
+            <p>{props.shoes[0].content}</p>
+            <p>{props.shoes[0].price}원</p>
+            <button className="btn btn-danger">주문하기</button>
+          </div>
+        </Row>
       </Container>
     </>
   );
--- online_store2023-12-20/src/routes/CustomRoute/index.js
+++ online_store2023-12-20/src/routes/CustomRoute/index.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Routes, Route, useLocation, Link } from "react-router-dom";
 import Pages from "pages";
+import { data } from "data";
 
 export default function CustomRoute() {
   let location = useLocation();
+  let [shoes] = useState(data);
 
   React.useEffect(() => {
     window.scrollTo(0, 0);
@@ -24,7 +26,7 @@ export default function CustomRoute() {
       </Navbar>
       <Routes>
         <Route path="/" element={<Pages.MainPage />} />
-        <Route path="/detail" element={<Pages.DetailPage />} />
+        <Route path="/detail" element={<Pages.DetailPage shoes={shoes} />} />
       </Routes>
     </>
   );
